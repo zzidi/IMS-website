8ff9715d7c5f3030e7e6ea4132de1ca9b248a2fa
zzidi <158415705+zzidi@users.noreply.github.com>
2024-02-01 14:47:43 +0800

    Delete dbconnects.php

--- dbconnects.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//connection database to the system
-	$dbcs=mysqli_connect("localhost","root","","accsrsrpt");
-	if(mysqli_connect_errno())
-	{
-		echo "Problem In Database Connection".mysqli_connect_error($dbc);
-	}
-?>
